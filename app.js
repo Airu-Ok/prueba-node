17283d7eb2afb6c86ff7289363ebb44ff73639ec
Airu-Ok <gutierrez.charcas97@gmail.com>
2021-10-19 00:50:49 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+function sumar (a, b){
+    return a + b
+}
+
+function restar (a, b){
+    return a - b
+}
+
+function multiplicar (a, b){
+    return a * b
+}
+
+function dividir (a, b){
+    return a / b
+}
+
+let resultado;
+
+resultado = sumar (1, 2);
+console.log('la suma es :' + resultado);
+
+resultado = restar (6, 2);
+console.log('la resta es :' + resultado);
+
+resultado = multiplicar (1, 5);
+console.log('la multiplicación es :' + resultado);
+
+resultado = dividir (12, 2);
+console.log('la divición es :' + resultado);
